272ecfee31a78ddf9427a8fdc2c8303da3d006db
cdrani <cdrani@users.noreply.github.com>
2019-03-05 11:15:37 -0700

    update .npm-init fields and dependencies versions

--- .npm-init.js
+++ .npm-init.js
@@ -1,4 +1,4 @@
-const url = 'https://github.com/cdrainxv/'
+const url = 'https://github.com/cdrani/'
 
 const pwd = () => {
   const path = process.env.PWD.split('/')
@@ -7,41 +7,31 @@ const pwd = () => {
 
 module.exports = {
   name: prompt('name', `${pwd()}`, name => `${name}`),
-  version: prompt('version', '0.0.1', version => `${version}`),
-  main: prompt('main file', 'index', main => `${main}.js`),
-  description: prompt('desc', 'verbal diarrhea', desc => `${desc}`),
+  version: prompt('version', '0.1.0', version => `${version}`),
+  main: prompt('main file', 'index.js', main => `${main}.js`),
+  description: prompt('description', 'awesome project', desc => `${desc}`),
   scripts: {
     clean: 'rimraf dist',
     build: 'npm run clean && webpack',
     serve: 'webpack-dev-server'
   },
-  keywords: prompt('keywords', 'node, webpack, react'),
+  keywords: prompt('keywords', 'node, webpack, react', list => list.split(' ')),
   dependencies: {
-    'prop-types': '^15.5.10',
-    react: '^15.6.1',
-    'react-dom': '^15.6.1'
+    'prop-types': '^15.7.2',
+    'react': '^16.8.3',
+    'react-dom': '^16.8.3'
   },
   devDependencies: {
-    'babel-core': '^6.26.0',
-    'babel-loader': '^7.1.2',
-    'babel-preset-env': '^1.6.0',
-    'babel-preset-react': '^6.24.1',
-    'css-loader': '^0.28.7',
-    'extract-text-webpack-plugin': '^3.0.0',
-    'html-webpack-plugin': '^2.30.1',
-    'postcss-loader': '^2.0.6',
-    'postcss-nested': '^2.1.2',
-    rimraf: '^2.6.2',
-    'style-loader': '^0.18.2',
-    webpack: '^3.6.0',
-    'webpack-dev-server': '^2.8.2'
+    'rimraf': '^2.6.3',
+    'webpack': '^4.29.6',
+    'webpack-dev-server': '^3.2.1'
   },
   repository: {
     type: prompt('source control', 'git', type => `${type}`),
     url: prompt('repo url', `${pwd()}`, repo => `${url}${repo}.git`)
   },
-  author: prompt('author', `cdrainxv`, author => `${author}`),
+  author: prompt('author', `cdrani`, author => `${author}`),
   license: prompt('MIT | ISC | BSD', 'MIT', license => `${license}`),
-  bugs: prompt('bugs', `${pwd()}`, issue => `${url}${issue}/issues`),
-  homepage: prompt('link', `${url}${pwd()}`, link => `${link}`)
+  bugs: prompt('bugs', `${pwd()}`, repo => `${url}${repo}/issues`),
+  homepage: prompt('homepage url', `${url}${pwd()}`, link => `${link}`)
 }
