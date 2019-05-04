2e05e50d0e87f7117aacaabaaa5da15d117d806d
cdrani <charlesdrani@gmail.com>
2019-05-03 19:55:04 -0600

    remove scripts, (dev)Dependencies fields

--- .npm-init.js
+++ .npm-init.js
@@ -10,22 +10,7 @@ module.exports = {
   version: prompt('version', '0.1.0', version => `${version}`),
   main: prompt('main file', 'index.js', main => `${main}.js`),
   description: prompt('description', 'awesome project', desc => `${desc}`),
-  scripts: {
-    'clean': 'rimraf dist',
-    'build': 'npm run clean && webpack',
-    'serve': 'webpack-dev-server'
-  },
-  keywords: prompt('keywords', 'node, webpack, react', list => list.split(' ')),
-  dependencies: {
-    'prop-types': '^15.7.2',
-    'react': '^16.8.3',
-    'react-dom': '^16.8.3'
-  },
-  devDependencies: {
-    'rimraf': '^2.6.3',
-    'webpack': '^4.29.6',
-    'webpack-dev-server': '^3.2.1'
-  },
+  keywords: prompt('keywords', 'node webpack react', list => list.split(' ')),
   repository: {
     type: prompt('source control', 'git', type => `${type}`),
     url: prompt('repo url', `${pwd()}`, repo => `${url}${repo}.git`)
